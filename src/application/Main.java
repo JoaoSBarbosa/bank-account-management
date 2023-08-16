0574ea0a65859d3ddf2484c91ed3bc34b41f94f4
João Barbosa <contato.jsbarbosa@gmail.com>
2023-08-16 19:15:32 -0300

    update Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,22 +10,25 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double deposit = 0;
+		Account ac;
 		System.out.print("Enter account number: ");
 		int account = sc.nextInt();
 		sc.nextLine();
+		
 		System.out.print("Enter account holder: ");
 		String holder = sc.nextLine();
+		
 		System.out.print("Is there na initial deposit (y/n)?");
 		char cond = sc.next().charAt(0);
+		
 		if(cond == 'y') {
 			System.out.print("Enter initial deposit value: ");
-			deposit = sc.nextDouble();
+			double deposit = sc.nextDouble();
+			ac = new Account(account,holder,deposit);
 		}else {
-			System.out.println();
+			ac = new Account(account,holder);
 		}
 		
-		Account ac = new Account(account,holder,deposit);
 		
 		System.out.println("Account data: \n"+ac);
 		System.out.println();
